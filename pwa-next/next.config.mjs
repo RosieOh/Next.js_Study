0521e4703e4c38ae49883d0d5ebce6970492cd7e
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-16 09:04:25 +0900

    ✨FEAT : next-compose-plugins, next-pwa 설정 추가

--- pwa-next/next.config.mjs
+++ pwa-next/next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPlugins from 'next-compose-plugins';
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+export default withPlugins(
+    [
+        [
+            withPWA,
+            {
+                pwa: {
+                    dest: "public",
+                },
+            },
+        ],
+        // 추가 플러그인 작성
+    ],
+    nextConfig
+);
